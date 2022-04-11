d13594f3c9cf6db984d27fe4cadff0199befa486
iamgauraav <iamgauraav@gmail.com>
2022-04-11 21:36:28 +0530

    Added UC1 : To analyze mood of user using mstesting

---
+++ MSTestForMoodAnalyzer/UnitTest1.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Mood_Analyzer_problem;
+
+namespace MSTestForMoodAnalyzer
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        //Arrangement
+        MoodAnalyzer moodAnalyzer = new MoodAnalyzer(); 
+
+        [TestMethod]
+        public void CheckMoodHappy()
+        {
+            //Action
+            string res = moodAnalyzer.CheckMood("Rishabh is Happy for his new laptop");
+            //Assert
+            Assert.AreEqual("happy", res);
+        }
+        [TestMethod]
+        public void CheckMoodSad()
+        {
+            //Action
+            string res = moodAnalyzer.CheckMood("Rishabh is Sad after getting bad marks");
+            //Assert
+            Assert.AreEqual("sad", res);
+        }
+    }
+}
--- Mood_Analyzer_problem/MoodAnalyzer.cs
+++ Mood_Analyzer_problem/MoodAnalyzer.cs
@@ -11,6 +11,8 @@ namespace Mood_Analyzer_problem
     {
         public string CheckMood(string msg)
         {
+            //msg : Rishabh is Happy for his new laptop
+            //msg : Rishabh is Sad after getting bad marks
             if (msg.Contains("happy" , StringComparison.OrdinalIgnoreCase))
             {
                 return "happy";
