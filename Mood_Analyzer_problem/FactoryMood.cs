9c702803442b6d63a5c4911f330548b23a1d6026
iamgauraav <iamgauraav@gmail.com>
2022-04-13 19:26:35 +0530

    Added UC5 : To create parameterized constructor using reflection

--- Mood_Analyzer_problem/FactoryMood.cs
+++ Mood_Analyzer_problem/FactoryMood.cs
@@ -20,5 +20,17 @@ namespace Mood_Analyzer_problem
             var myObj = mycon.Invoke(new object[0]);
             return myObj;
         }
+        public object GetObjectByParameterizedConstructor(string msg)
+        {
+            //reff     var              con
+            // MyClass myClsObj = new MyClass()
+            //get constructor
+            ConstructorInfo[] constructor = type.GetConstructors();
+            ConstructorInfo mycon = constructor[0];
+            //it will create obj and assign memory in new obj
+            //invoke is a method inside this constructor infomation
+            var myObj = Activator.CreateInstance(mycon.DeclaringType,msg);
+            return myObj;
+        }
     }
 }
