d479908f80a281a95435b569ff5972c79513201b
Chetan Deore <deorec346@gmail.com>
2022-05-01 15:56:45 +0530

    AnalyzeMood

---
+++ MSTestForMoodAnalyzer/UnitTest1.cs
@@ -0,0 +1,80 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Mood_Analyzer_problem;
+
+namespace MSTestForMoodAnalyzer
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        //Arrangement
+        MoodAnalyzer moodAnalyzer = new MoodAnalyzer(); 
+
+        [TestMethod]
+        public void CheckMoodHappy()
+        {
+            //Action
+            string res = moodAnalyzer.CheckMood("Rishabh is Happy for his new laptop");
+            //Assert
+            Assert.AreEqual("happy", res);
+        }
+        [TestMethod]
+        public void CheckMoodSad()
+        {
+            //Action
+            string res = moodAnalyzer.CheckMood("Rishabh is Sad after getting bad marks");
+            //Assert
+            Assert.AreEqual("sad", res);
+        }
+        [TestMethod]
+        public void CheckNullException()
+        {
+            try
+            {
+                //Action
+                string res = moodAnalyzer.CheckMood(null);
+            }
+            catch(CustomMoodAnalyzerException mEx)
+            {
+                //Assert
+                Assert.AreEqual(ExceptionType.NULL_VALUE, mEx.extype);
+                Assert.AreEqual("message is null", mEx.Message);
+
+            }
+
+        }
+        [TestMethod]
+        public void CheckEmptyExcep()
+        {
+            try
+            {
+                //Action
+                string res = moodAnalyzer.CheckMood("");
+            }
+            catch (CustomMoodAnalyzerException mEx)
+            {
+                //Assert
+                Assert.AreEqual(ExceptionType.EMPTY_TYPE, mEx.extype);
+                Assert.AreEqual("provided message is empty", mEx.Message);
+
+            }
+
+        }
+        [TestMethod]
+        public void CheckObjWithDefaultConstructor()
+        {
+            FactoryMood<MoodAnalyzer> myFact = new FactoryMood<MoodAnalyzer>();
+            var res_obj = myFact.GetObjectByDefaultConstructor();
+
+            Assert.IsInstanceOfType(res_obj,typeof(MoodAnalyzer));
+        }
+        [TestMethod]
+        public void InvokedMethod()
+        {
+            FactoryMood<MoodAnalyzer> myFact = new FactoryMood<MoodAnalyzer>();
+
+            string result = myFact.InvokeMood("Rishabh is happy after getting bad marks");
+            Assert.AreEqual("happy", result);
+        }
+
+    }
+}
---
+++ Mood_Analyzer_problem/MoodAnalyzer.cs
@@ -0,0 +1,70 @@
+﻿using System;
+namespace Mood_Analyzer_problem
+{
+    public enum  Mood
+    {
+        Happy,
+        sad,
+    }
+
+    public class MoodAnalyzer
+    {
+        public string message;
+
+        public MoodAnalyzer()
+        {
+            this.message = "";
+        }
+
+        public MoodAnalyzer(string msg)
+        {
+            this.message = msg; 
+        }
+        public string CheckMood(string msg)
+        {
+            message = msg;
+            //msg : Rishabh is Happy for his new laptop
+            //msg : Rishabh is Sad after getting bad marks
+            if (message == null)
+            {
+                throw new CustomMoodAnalyzerException("message is null", ExceptionType.NULL_VALUE);
+            }
+            else if (message.Length < 1)
+            {
+                throw new CustomMoodAnalyzerException("provided message is empty", ExceptionType.EMPTY_TYPE);
+            }
+
+            if (message.Contains("happy" , StringComparison.OrdinalIgnoreCase))
+            {
+                return "happy";
+            }
+             if(message.Contains("sad", StringComparison.OrdinalIgnoreCase))
+            {
+                return "sad";
+            }
+            return null;
+        }
+
+        public string CheckMoodTwo()
+        {
+            if (message == null)
+            {
+                throw new CustomMoodAnalyzerException("message is null", ExceptionType.NULL_VALUE);
+            }
+            else if (message.Length < 1)
+            {
+                throw new CustomMoodAnalyzerException("provided message is empty", ExceptionType.EMPTY_TYPE);
+            }
+
+            if (message.Contains("happy", StringComparison.OrdinalIgnoreCase))
+            {
+                return "happy";
+            }
+            if (message.Contains("sad", StringComparison.OrdinalIgnoreCase))
+            {
+                return "sad";
+            }
+            return null;
+        }
+    }
+}
