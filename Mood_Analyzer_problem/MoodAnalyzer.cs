dbb1481e42de9747a9ffe23f23820dd71177868e
Chetan Deore <deorec346@gmail.com>
2022-05-02 18:44:57 +0530

    ExceptionalNull

---
+++ Mood_Analyzer_problem/MoodAnalyzer.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace MoodAnalyzerProblem
+{
+    public class MoodAnalyzer
+    {
+        public string message;
+        public MoodAnalyzer()
+        {
+
+        }
+            /// Parameterised Constructor.
+            public MoodAnalyzer(string message)
+        {
+            this.message = message;
+        }
+
+        ///UC1:-Analyse Happy And Sad Mood
+        ///if messege Contain Sad then return Sad
+        ///if message contain Happy then return Happy
+
+        public string AnalyzeMood()
+        {
+            try // Block of Code to be tested for error while it is being executed
+            {
+                if (this.message.Contains("sad"))//Contains():- This method is used to check whether the substring occurs within a given string or not.
+                    return "sad";
+                else if (this.message.Contains("happy"))
+                    return "happy";
+                else if (this.message.Contains(null))
+                    return "happy";
+            }
+            catch // Block of code to be Executed if an error in the block
+            {
+                return "happy";
+            }
+            return null; // Value Return for all code path (AnalyzeMood)
+        }
+
+    }
+}
---
+++ Mood_Analyzer_problem/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoodAnalyzerProblem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Welcome Message
+            Console.WriteLine("=-=-=-=-=-=-=-=Welcome To The Mood Analyzer Problem=-=-=-=-=-=-=-=");
+
+            //UC1:-Respond Happy and Sad Mood
+            MoodAnalyzer analyser = new MoodAnalyzer("I am in sad Mood");
+            Console.WriteLine("I am in Sad Mood:- " + analyser.AnalyzeMood());
+            MoodAnalyzer analysermood = new MoodAnalyzer("I am in happy Mood");
+            Console.WriteLine("I am in Any Mood:- " + analysermood.AnalyzeMood());
+
+            //UC2:-Null mood Should Return Happy
+            MoodAnalyzer moodAnalyser = new MoodAnalyzer("");
+            Console.WriteLine("Given Null Message:- " + moodAnalyser.AnalyzeMood());
+
+            Console.ReadLine();
+        }
+    }
+}
